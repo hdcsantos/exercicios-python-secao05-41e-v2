a57bf80a18971d18b06620f991e76630548c39fc
Henrique Santos <riquesantos9@gmail.com>
2019-07-16 11:08:33 -0300

    sessao5_01

---
+++ 01.py
@@ -0,0 +1,9 @@
+print("Número maior")
+
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+
+if n1 > n2:
+    print(f'{n1} é o maior número')
+else:
+    print(f'{n2} é o maior número')
