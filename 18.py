b17d1edd5bf06c5dab861a67e685938846fecb22
Henrique Santos <riquesantos9@gmail.com>
2019-07-20 15:46:24 -0300

    operacoes matematicas com função

---
+++ 18.py
@@ -0,0 +1,19 @@
+import op_matematicas
+
+print('Operacoes matematicas')
+
+operacao = input('Qual operacão matemática voce deseja fazer, dentre as principais? ')
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+
+if operacao == 'soma':
+    # r = op_matematicas.soma(n1, n2)
+    print(f'Resultado da soma é {op_matematicas.soma(n1, n2)}')
+elif operacao == 'subtracao':
+    print(f'Resultado da subtração é {op_matematicas.subtracao(n1, n2)}')
+elif operacao == 'multiplicacao':
+    print(f'Resultado da multiplicação é {op_matematicas.multiplicacao(n1, n2)}')
+elif operacao == 'divisao':
+    print(f'Resultado da divisão é {op_matematicas.divisao(n1, n2)}')
+else:
+    print("Operação desconhecida !!!")
