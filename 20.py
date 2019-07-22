8e56083919dbae61c95a6d3eccc39ddd2b658795
Henrique Santos <riquesantos9@gmail.com>
2019-07-22 11:48:37 -0300

    Alteracao de lógica e adptacao ao PEP8

--- 20.py
+++ 20.py
@@ -3,13 +3,13 @@ a = int(input('Infome o valor do lado a: '))
 b = int(input('Infome o valor do lado b: '))
 c = int(input('Infome o valor do lado c: '))
 
-if (b + c) > a and (c + a) > b and (a + b) > c :
-    print('De acordo com as medidas, vc criou um triângulo !!!')
+if a < (b + c) and b < (c + a) and c < (a + b):
+    print('De acordo com os valores, vc criou um triângulo! Agora vamos saber qual é o seu tipo.')
     if a == b and b == c and c == a:
-        print('O triangulo criado é equilátero')
+        print('O triangulo criado é equilátero, pois seus tres lados são iguais!')
     elif a == b or c == a or b == c:
-        print('O triangulo criado é isóceles')
-    elif a != b and b != c and c != a:
-        print('O triangulo criado é escaleno')
+        print('O triangulo criado é isóceles, pois dois de seus lados são iguais!')
+    else:  # elif a != b and b != c and c != a:
+        print('O triangulo criado é escaleno, pois seus tres lados são difeirentes!')
 else:
-  print('De acordo com as medidas, vc não criou um triângulo !!!!')
+    print('De acordo com as medidas, vc não criou um triângulo !!!!')
