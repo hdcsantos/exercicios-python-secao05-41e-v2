533c7f71a60f050f49c3eb89f117b91fd5d5cda8
Henrique Santos <52087901+hdcsantos@users.noreply.github.com>
2019-07-23 21:14:47 -0300

    Update 25.py

--- 25.py
+++ 25.py
@@ -19,17 +19,14 @@ else:
     if delta < 0:
         print(f"Nas resoluções em que o valor do discriminante (delta) é menor que zero ({delta}), isto é,\n"
               "o número é negativo, a equação não possui raízes reais. Programa encerrado !!!")
-
     elif delta == 0:
         # raiz_delta = math.sqrt(delta)
         x1 = ((-b) + (delta ** 0.5)) / (2 * a)
         print(f"Raiz única x' = x'' = {x1}")
         print("Fim do programa !!!")
-
     elif delta >= 0:
         x1 = ((-b) + (delta ** 0.5))/(2 * a)
         x2 = (-b - (math.sqrt(delta))) / (2 * a)
-
         print("Valor de raiz x' = " + str(x1))
         print("Valor de raiz x'' = " + str(x2))
         print("Fim do programa !!!")
